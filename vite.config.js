d160ac272f6065785aa934875f597c1392b1265d
endayshebocah <endayshebocah@gmail.com>
2025-08-30 10:23:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kokuo-Tc/', // Baris ini memperbaiki masalah layar putih
+  base: '/Kokuo-Tc/', // Pastikan ini sesuai dengan nama repositori Anda
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  server: {
+    open: true,
+  },
 })
 
